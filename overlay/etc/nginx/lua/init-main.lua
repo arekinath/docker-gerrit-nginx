f07866349c10f1be12641713e438face37907022
Alex Wilson <alex.wilson@joyent.com>
2016-07-13 14:12:48 -0700

    Make regen-ssh-config use a temp dir per process too

--- overlay/etc/nginx/lua/init-main.lua
+++ overlay/etc/nginx/lua/init-main.lua
@@ -1,6 +1,5 @@
-local state = ngx.shared.state
-
 function acme_check()
+	local state = ngx.shared.state
 	assert(ngx.timer.at(600, acme_check))
 	if state:get('in_check') ~= nil then
 		return
@@ -17,6 +16,7 @@ function acme_check()
 end
 
 function acme_renew()
+	local state = ngx.shared.state
 	ngx.log(ngx.WARN, 'attempting to renew ACME cert')
 	local dir = '/tmp/acme-renew-' .. ffi.C.getpid()
 	os.execute('rm -fr ' .. dir)
@@ -29,8 +29,8 @@ function acme_renew()
 	if ret ~= 0 then
 		ngx.log(ngx.ERR,
 		    'failed to renew ACME cert, will try again in 5min')
-		assert(ngx.timer.at(300, acme_check))
 		state:delete('in_check')
+		assert(ngx.timer.at(300, acme_check))
 		return
 	end
 	assert(os.execute(
@@ -49,21 +49,28 @@ end
 local gen = require('genupstream')
 
 function regen_ssh_config()
+	local state = ngx.shared.state
 	assert(ngx.timer.at(300, regen_ssh_config))
 	if state:get('in_regen_ssh') ~= nil then
 		return
 	end
 	state:set('in_regen_ssh', true)
+	local dir = '/tmp/regen-sshconfig-' .. ffi.C.getpid()
+	os.execute('rm -fr ' .. dir)
+	assert(os.execute('mkdir -p ' .. dir) == 0)
 	ngx.log(ngx.NOTICE, 'checking for ssh upstream changes in DNS')
 	gen.regen_resolved(os.getenv('GERRIT_HOST'), os.getenv('SSH_PORT'),
-	    '/tmp/upstreams-ssh.conf')
-	local ret = os.execute('diff /etc/nginx/upstreams-ssh.conf /tmp/upstreams-ssh.conf >/dev/null')
+	    dir .. '/upstreams-ssh.conf')
+	local ret = os.execute('diff /etc/nginx/upstreams-ssh.conf ' ..
+	    dir .. '/upstreams-ssh.conf >/dev/null')
 	if ret ~= 0 then
 		ngx.log(ngx.NOTICE, 'ssh upstreams have changed, reloading config')
-		assert(os.execute('mv /tmp/upstreams-ssh.conf /etc/nginx/upstreams-ssh.conf') == 0)
+		assert(os.execute('mv ' .. dir .. '/upstreams-ssh.conf /etc/nginx/upstreams-ssh.conf') == 0)
+		assert(os.execute('rm -fr ' .. dir) == 0)
 		state:delete('in_regen_ssh')
 		assert(os.execute('kill -HUP ' .. master_pid))
 	else
+		assert(os.execute('rm -fr ' .. dir) == 0)
 		state:delete('in_regen_ssh')
 	end
 end
